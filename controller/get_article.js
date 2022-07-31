ed87fd5486a248ddc8b003c0d4d0eb3e940ea2c4
datispham <datistpham@gmail.com>
2022-08-01 02:05:35 +0700

    commit message

--- controller/get_article.js
+++ controller/get_article.js
@@ -13,7 +13,6 @@ const apiGetArticle = async (request, response_) => {
   try {
     const response = await fetch("https://blockchainwhispers.com/api/get-news");
     const body = await response.json();
-
     await Promise.all(
       body?.map(async (item) => {
         const res = await translate({ ...item, image: "" }, { to: "ko" })
