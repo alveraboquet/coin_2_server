7669f02327eb261548ae10fc679df7ad178a2f22
datispham <datistpham@gmail.com>
2022-07-31 22:29:42 +0700

    commit message

--- app.js
+++ app.js
@@ -3,139 +3,142 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import fetch from "node-fetch"
 import apiGetArticle from "./controller/get_article.js";
 import redis from "redis"
 
+let arr= []
 const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://datistpham27.github.io/coin_2/", "http://localhost:3000/"],
     methods: ["GET", "POST"],
   })
 );
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', "https://datistpham27.github.io/coin_2/"]);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  })
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:3000", "https://datistpham27.github.io/coin_2/"],
     methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
   },
 });
 // export const client= redis.createClient()
 // await client.connect()
 
+const { streamNormalized, normalizeLiquidations, combine } = require('tardis-dev')
+
+// let's monitor BTC perpetuals swaps only
 const monitoredExchanges = [
-  { id: "ftx", symbols: ["BTC-PERP"] },
-  { id: "bitmex", symbols: ["XBTUSD"] },
-  { id: "deribit", symbols: ["BTC-PERPETUAL"] },
-  { id: "binance-futures", symbols: ["BTCUSDT"] },
-  { id: "binance-delivery", symbols: ["BTCUSD_PERP"] },
-  { id: "bitfinex-derivatives", symbols: ["BTCF0:USTF0"] },
-  { id: "cryptofacilities", symbols: ["PI_XBTUSD"] },
-  { id: "huobi-dm-swap", symbols: ["BTC-USD"] },
-  { id: "ftx", symbols: ["BTC-PERP"] },
-  { id: "bitmex", symbols: ["XBTUSD"] },
-];
+  { id: 'ftx', symbols: ['BTC-PERP'] },
+  { id: 'bitmex', symbols: ['XBTUSD'] },
+  { id: 'deribit', symbols: ['BTC-PERPETUAL'] },
+  { id: 'binance-futures', symbols: ['BTCUSDT'] },
+  { id: 'binance-delivery', symbols: ['BTCUSD_PERP'] },
+  { id: 'bitfinex-derivatives', symbols: ['BTCF0:USTF0'] },
+  { id: 'cryptofacilities', symbols: ['PI_XBTUSD'] },
+  { id: 'huobi-dm-swap', symbols: ['BTC-USD'] }
+]
 
 async function monitorLiquidations() {
-  const monitoredExchangesLiquidationsStreams = monitoredExchanges.map(
-    (exchange) => {
-      return streamNormalized(
-        {
-          exchange: exchange.id,
-          symbols: exchange.symbols,
-          timeoutIntervalMS: 30 * 60 * 1000,
-        },
-        normalizeLiquidations
-      );
-    }
-  );
+  const monitoredExchangesLiquidationsStreams = monitoredExchanges.map((exchange) => {
+    return streamNormalized(
+      {
+        exchange: exchange.id,
+        symbols: exchange.symbols,
+        timeoutIntervalMS: 30 * 60 * 1000
+      },
+      normalizeLiquidations
+    )
+  })
 
-  const combinedLiquidationStream = combine(
-    ...monitoredExchangesLiquidationsStreams
-  );
+  const combinedLiquidationStream = combine(...monitoredExchangesLiquidationsStreams)
 
-  console.log("Liquidations monitor started...");
+  console.log('Liquidations monitor started...')
 
   for await (const liquidation of combinedLiquidationStream) {
-    formatLiquidation(liquidation);
+    console.log(formatLiquidation(liquidation))
   }
 }
 
 const meta = {
   ftx: {
-    name: "FTX",
+    name: 'FTX',
     contractMultiplier: 1,
-    inverse: true,
+    inverse: false
   },
   bitmex: {
-    name: "BitMEX",
+    name: 'BitMEX',
     contractMultiplier: 1,
-    inverse: true,
+    inverse: true
   },
   deribit: {
-    name: "Deribit",
+    name: 'Deribit',
     contractMultiplier: 1,
-    inverse: true,
+    inverse: true
   },
-  "binance-futures": {
-    name: "Binance USDT Futures",
+  'binance-futures': {
+    name: 'Binance USDT Futures',
     contractMultiplier: 1,
-    inverse: true,
+    inverse: false
   },
-  "binance-delivery": {
-    name: "Binance COIN Futures",
+  'binance-delivery': {
+    name: 'Binance COIN Futures',
     contractMultiplier: 1,
-    inverse: true,
+    inverse: true
   },
-  "bitfinex-derivatives": {
-    name: "Bitfinex Derivatives",
+  'bitfinex-derivatives': {
+    name: 'Bitfinex Derivatives',
     contractMultiplier: 1,
-    inverse: true,
+    inverse: false
   },
   cryptofacilities: {
-    name: "Kraken Futures",
+    name: 'Kraken Futures',
     contractMultiplier: 1,
-    inverse: true,
+    inverse: true
   },
-  "huobi-dm-swap": {
-    name: "Huobi Swap",
+  'huobi-dm-swap': {
+    name: 'Huobi Swap',
     contractMultiplier: 100,
-    inverse: true,
-  },
-};
+    inverse: true
+  }
+}
 
-const usdCurrencyFormatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-});
-let arr = [];
+const usdCurrencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+})
 
 function formatLiquidation(liquidation) {
-  const { name, contractMultiplier, inverse } = meta[liquidation.exchange];
-  const position = liquidation.side === "sell" ? "long" : "short";
-  const price = usdCurrencyFormatter.format(liquidation.price);
-  let normalizedAmount = liquidation.amount * contractMultiplier;
+  const { name, contractMultiplier, inverse } = meta[liquidation.exchange]
+  const position = liquidation.side === 'sell' ? 'long' : 'short'
+  const price = usdCurrencyFormatter.format(liquidation.price)
+  let normalizedAmount = liquidation.amount * contractMultiplier
   if (inverse === false) {
-    normalizedAmount = normalizedAmount * liquidation.price;
+    normalizedAmount = normalizedAmount * liquidation.price
   }
-  const minSize = 5000;
-  if (normalizedAmount < minSize) {
-    return;
-  }
-  const liquidatedAmunt = usdCurrencyFormatter.format(normalizedAmount);
-  const timestamp = liquidation.timestamp.toISOString();
-  const direction = liquidation.side === "sell" ? "📉" : "📈";
+  const liquidatedAmunt = usdCurrencyFormatter.format(normalizedAmount)
+  const timestamp = liquidation.timestamp.toISOString()
+  const direction = liquidation.side === 'sell' ? '📉' : '📈'
   console.log(
     `${direction} ${name} liquidated ${position} ${liquidation.symbol} position` +
-      ` at ${price}: ${liquidation.side} ${liquidatedAmunt}, timestmap: ${timestamp}`
-  );
-  arr.push(
+    `at ${price}: ${liquidation.side} ${liquidatedAmunt}, timestmap: ${timestamp}`
+  )
+  return arr.push(
     `${direction} ${name} liquidated ${position} ${liquidation.symbol} position` +
       ` at ${price}: ${liquidation.side} ${liquidatedAmunt}, timestmap: ${timestamp}`
   );
 }
+
+monitorLiquidations()
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -143,8 +146,6 @@ app.get("/api/v1/get/article", apiGetArticle)
 server.listen(process.env.PORT || 4000, () => {
   console.log("Listening on port 4000");
 });
-monitorLiquidations();
-
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("get_data", (data) => {
--- controller/get_article.js
+++ controller/get_article.js
@@ -1,32 +1,38 @@
-import fetch from "node-fetch"
-import translate from "translate-google"
+import fetch from "node-fetch";
+import translate from "translate-google";
 // import { client } from "../app.js"
 
-const apiGetArticle= async (request, response_)=> {
-    // client.get("article")
-    // .then(async res=> {
-    //     if(JSON.parse(res)) {
-    //         return response_.json(JSON.parse(res))
-    //     }
-    //     else {
-            try {
-                const response= await fetch("https://blockchainwhispers.com/api/get-news")
-                const body= await response.json()
-                let arr= []
-                const results= await Promise.all(body?.map(async (item)=> {
-                    const res = await translate({...item, image: ""}, {to: 'ko'})
-                    return arr.push(res)
-                }))
-                // client.set("article", JSON.stringify(arr))
-                // client.EXPIRE("article", 3600)
-                return response_.json(arr)
-                
-            } catch (error) {
-                return console.log(error)
-            }
-    //     }
-    // })
-    
-}
+const apiGetArticle = async (request, response_) => {
+  // client.get("article")
+  // .then(async res=> {
+  //     if(JSON.parse(res)) {
+  //         return response_.json(JSON.parse(res))
+  //     }
+  //     else {
+  let arr = [];
+  try {
+    const response = await fetch("https://blockchainwhispers.com/api/get-news");
+    const body = await response.json();
 
-export default apiGetArticle
+    await Promise.all(
+      body?.map(async (item) => {
+        const res = await translate({ ...item, image: "" }, { to: "ko" })
+        return arr.push(res);
+      })
+    )
+      // client.set("article", JSON.stringify(arr))
+      // client.EXPIRE("article", 3600)
+      .then(() => {
+        return response_.json(arr);
+      })
+      .catch((err) => {
+        return response_.json([])
+      });
+  } catch (error) {
+    return response_.json([])
+  }
+  //     }
+  // })
+};
+
+export default apiGetArticle;
