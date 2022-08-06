83ab0083c8033ea289af9b5fbe6b85aaf682f373
datispham <datistpham@gmail.com>
2022-08-06 23:52:47 +0700

    final commit

--- controller/get_liquidation.js
+++ controller/get_liquidation.js
@@ -11,7 +11,7 @@ const monitoredExchanges = [
   { id: "bitfinex-derivatives", symbols: ["BTCF0:USTF0", "ETHF0:USTF0"] },
   { id: "cryptofacilities", symbols: ["PI_XBTUSD", "PI_ETHUSD"] },
   { id: "huobi-dm-swap", symbols: ["BTC-USD", "ETH-USD"] },
-  { id: "bybit", symbols: ["BTCUSDT", "ETHUSDT"] },
+  { id: "bybit", symbols: ["BTCUSD", "ETHUSD"] },
 ];
 
 async function monitorLiquidations() {
@@ -83,11 +83,6 @@ const meta = {
   bybit: {
     name: "Bybit",
     contractMultiplier: 100,
-    inverse: true,
-  },
-  bybit: {
-    name: "Bybit",
-    contractMultiplier: 1,
     inverse: true,
   },
 };
