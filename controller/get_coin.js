e9746dfac797ec95f3f5a42878cd6e27ca57d645
datispham <datistpham@gmail.com>
2022-08-10 00:49:19 +0700

    commit

--- controller/get_coin.js
+++ controller/get_coin.js
@@ -1,12 +1,11 @@
-let arr = [];
+const arr = [];
 const apiGetCoin = (req, res) => {
   var body = "";
   req.on("data", function (chunk) {
     body += chunk;
   });
   req.on("end", function () {
-    arr.push(JSON.parse(body))
-    console.log("body: " + JSON.parse(body));
+    arr.push(body)
   });
   return res.send(arr);
 };
