2e444e1194974896d67775526cdbf49f998410e6
datispham <datistpham@gmail.com>
2022-08-11 04:19:54 +0700

    p

--- route/route.js
+++ route/route.js
@@ -4,15 +4,16 @@ import apiGetCoin from "../controller/get_coin.js";
 import get_liquidation from "../controller/get_liquidation.js";
 import { apiv2Liquidation } from "../controller/get_liquidation2.js";
 import { apiCoinToClient } from "../controller/to_client.js";
+import cors from "cors"
 
 const router= express.Router()
 router.get("/", (req, res) => {
     res.send("");
   });
-router.get("/api/v1/get/article", apiGetArticle);
-router.get("/api/v1/get/liquidation", get_liquidation);
-router.get("/api/v2/get/liquidation", apiv2Liquidation)
-router.post("/api/v2/get/get_coin", apiGetCoin)
-router.get("/api/v1/coin/get", apiCoinToClient)
+router.get("/api/v1/get/article", cors(), apiGetArticle);
+router.get("/api/v1/get/liquidation", cors(), get_liquidation);
+router.get("/api/v2/get/liquidation", cors(), apiv2Liquidation)
+router.post("/api/v2/get/get_coin", cors(),apiGetCoin)
+router.get("/api/v1/coin/get", cors(),apiCoinToClient)
 
 export default router
