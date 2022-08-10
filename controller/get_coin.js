55cdf5042664f4ab2b2b0aaed0a2bf81e98eba7e
datispham <datistpham@gmail.com>
2022-08-10 22:43:19 +0700

    commit

--- controller/get_coin.js
+++ controller/get_coin.js
@@ -1,4 +1,5 @@
 import _ from "lodash"
+import moment from "moment"
 
 const arr = []
 const arr2= []
@@ -10,7 +11,10 @@ const apiGetCoin = (req, res) => {
   });
   req.on("end", function () {
     arr.push(body)
-    arr?.filter(item=> item?.length > 0).map(item=> arr2?.push(JSON?.parse(item)))
+    arr?.filter(item=> item?.length > 0).map(item=> {
+      JSON.parse(item)["time"]= moment(new Date()).valueOf()
+      return arr2?.push(JSON?.parse(item))
+    })
   });
   return res.send(_.uniqWith(arr2, _.isEqual).slice(0, 50).reverse())
 }
